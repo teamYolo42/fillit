3cfcd245f5eccce673067a3a1282e405feeac138
Unknown <vgauther@student.42.fr>
2017-11-29 14:54:42 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ lib.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lib.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vgauther <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/29 13:53:44 by vgauther          #+#    #+#             */
+/*   Updated: 2017/11/29 14:47:56 by vgauther         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIB_H
+# define LIB_H
+
+# include <stdlib.h>
+# include <string.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include "libft.h"
+
+# define BUF_SIZE 1000
+
+int	ft_error(char *str);
+
+
+#endif
